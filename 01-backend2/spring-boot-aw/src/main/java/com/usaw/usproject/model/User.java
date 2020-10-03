94959fd8cda44dc7caf1fdce487fde979f2dda95
Andrzej <andrzejwojcicki@onet.eu>
2020-10-03 13:56:49 +0200

    authentication jwt added

--- 01-backend2/spring-boot-aw/src/main/java/com/usaw/usproject/model/User.java
+++ 01-backend2/spring-boot-aw/src/main/java/com/usaw/usproject/model/User.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
@@ -43,4 +44,16 @@ public class User {
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
     private Set<Rating> ratings;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(	name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+    public User(String username, String email, String encode) {
+    this.userName = username;
+    this.email = email;
+    this.password = encode;
+    }
 }
--- 01-backend2/spring-boot-aw/src/main/java/com/usaw/usproject/repository/CommentRepository.java
+++ 01-backend2/spring-boot-aw/src/main/java/com/usaw/usproject/repository/CommentRepository.java
@@ -4,6 +4,7 @@ import com.usaw.usproject.model.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+
 @RepositoryRestResource(collectionResourceRel = "commentsOfRecipe", path = "recipe-comments")
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 }
